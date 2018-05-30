e18eaa511058320ed63ab13ad731ec7cee5e777e
SupJoeeeeeey <joeyjiao1992@gmail.com>
2018-05-30 23:23:57 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,14 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    location: "广州市",
+    weatherPic: "../../images/cloudy-bg.png"
+  },
+  //事件处理函数
+  onLoad: function () {
+
+  }
+})
