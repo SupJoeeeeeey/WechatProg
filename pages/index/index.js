4bbd503dffb7b3c2338f5c4c0a21eefa860599eb
SupJoeeeeeey <joeyjiao1992@gmail.com>
2018-06-04 08:38:58 +0800

    new commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,10 @@ const app = getApp()
 Page({
   data: {
     location: "广州市",
-    weatherPic: "../../images/cloudy-bg.png"
+    weatherPic: "../../images/cloudy-bg.png",
+    temp: "17℃",
+    detail:"晴天",
+    weatherIcon: "../../images/sunny-icon.png"
   },
   //事件处理函数
   onLoad: function () {
