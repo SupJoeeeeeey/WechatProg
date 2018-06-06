5f907f109ba42b7c8ce5860aee8eb15973e4a432
Jiao Mengnan <mengnan.jiao@siemens.com>
2018-06-06 15:18:23 +0800

    add scroll view of forecast

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,12 +7,12 @@ let map = new QQMapWx({
   key: "DVDBZ-KY6LK-QPIJF-ACQ4R-SIFLK-6RFKD"
 })
 
-const weatherMap = {
-  "阴" : "",
-  "多云" : "",
-  "晴" : "",
-  "" : ""
-}
+// const weatherMap = {
+//   "阴" : "",
+//   "多云": "../../images/cloudy-bg.png",
+//   "晴" : "sunny-bg.png",
+//   "" : ""
+// }
 
 Page({
   data: {
@@ -20,13 +20,13 @@ Page({
     weatherPic: "../../images/cloudy-bg.png",
     temp: "17℃",
     detail:"晴天",
-    weatherIcon: "../../images/晴.png"
+    weatherIcon: "../../images/晴.png",
+    forecasts:[]
   },
   //事件处理函数
   onLoad: function () {
     wx.getLocation({
       success: res => {
-        console.log(res)
         map.reverseGeocoder({
           location: {
             latitude: res.latitude,
@@ -64,23 +64,34 @@ Page({
           detail: res.data.result.weather_curr,
           weatherIcon: "../../images/" + res.data.result.weather_curr + ".png"
         })
-        console.log(res)
       }
     })
   },
   setForecast: function(){
-    // wx.request({
-    //   url: 'https://www.sojson.com/open/api/weather/json.shtml',
-    //   data: {
-    //     city: '上海市'
-    //   },
-    //   success: res => {
-    //     this.setData({
-    //       temp: res.data.data.wendu + "℃"
-    //     })
-    //     console.log(res)
-    //   }
-    // })
+    wx.request({
+      url: 'http://api.k780.com',
+      data: {
+        app: "weather.future",
+        weaid: this.data.location,
+        appkey: "34189",
+        sign: "00a9dc0ce3a80506ee73f982df8b59db",
+        format: "json"
+      },
+      success: res => {
+        let arr = []
+        res.data.result.forEach(function(item, index){
+          let data = {
+            date: index === 0?"今天":item.week,
+            icon: "../../images/" + "晴" + ".png",
+            temp: item.temperature
+          }
+          arr.push(data)
+        })
+        this.setData({
+          forecasts: arr
+        })
+      }
+    })
     console.log("ok")
   }
 })
