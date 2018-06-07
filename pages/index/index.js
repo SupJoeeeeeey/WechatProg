5c33e609eb988cd9a4b5bbdce5460efc564e2d8b
Jiao Mengnan <mengnan.jiao@siemens.com>
2018-06-07 13:07:29 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,17 +7,42 @@ let map = new QQMapWx({
   key: "DVDBZ-KY6LK-QPIJF-ACQ4R-SIFLK-6RFKD"
 })
 
-// const weatherMap = {
-//   "阴" : "",
-//   "多云": "../../images/cloudy-bg.png",
-//   "晴" : "sunny-bg.png",
-//   "" : ""
-// }
+const weatherMap = {
+  "阴" : "阴背景",
+  "多云": "多云背景",
+  "晴" : "晴背景",
+  "雾": "阴背景",
+  "浓雾": "阴背景",
+  "强浓雾": "阴背景",
+  "特强浓雾": "阴背景",
+  "霾": "阴背景",
+  "中度霾": "阴背景",
+  "重度霾": "阴背景",
+  "严重霾": "阴背景",
+  "浮尘": "阴背景",
+  "扬沙": "阴背景",
+  "沙尘暴": "阴背景",
+  "特强浓雾": "阴背景",
+  "小雨": "小雨背景",
+  "阵雨": "小雨背景",
+  "冻雨": "小雨背景",
+  "雷阵雨": "暴雨背景",
+  "暴雨": "暴雨背景",
+  "中雨": "暴雨背景",
+  "大雨": "暴雨背景",
+  "大暴雨": "暴雨背景",
+  "特大暴雨": "暴雨背景",
+  "小雪": "雪背景",
+  "雨夹雪": "雪背景",
+  "阵雪": "雪背景",
+  "中雪": "雪背景",
+  "暴雪": "雪背景"
+}
 
 Page({
   data: {
     location: "上海",
-    weatherPic: "../../images/cloudy-bg.png",
+    weatherPic: "../../images/晴背景.png",
     temp: "17℃",
     detail:"晴天",
     weatherIcon: "../../images/晴.png",
@@ -46,9 +71,13 @@ Page({
       }
     })
     this.setTemp()
-    this.setForecast()
   },
-  setTemp: function(){
+  onPullDownRefresh: function(){
+    this.setTemp(function(){
+      wx.stopPullDownRefresh()
+    })
+  },
+  setTemp: function(cb){
     wx.request({
       url: 'http://api.k780.com',
       data: {
@@ -59,15 +88,22 @@ Page({
         format: "json"
       },
       success: res => {
+        let weather = weatherMap[res.data.result.weather_curr]
+        console.log(weather)
+        if(weather === null || weather === ""){
+          weather = "晴背景"
+        } 
         this.setData({
           temp: res.data.result.temp_curr + "℃",
           detail: res.data.result.weather_curr,
-          weatherIcon: "../../images/" + res.data.result.weather_curr + ".png"
+          weatherIcon: "../../images/" + res.data.result.weather_curr + ".png",
+          weatherPic: "../../images/" + weather + ".png"
         })
+        this.setForecast(cb)
       }
     })
   },
-  setForecast: function(){
+  setForecast: function(cb){
     wx.request({
       url: 'http://api.k780.com',
       data: {
@@ -82,7 +118,7 @@ Page({
         res.data.result.forEach(function(item, index){
           let data = {
             date: index === 0?"今天":item.week,
-            icon: "../../images/" + "晴" + ".png",
+            icon: "../../images/" + item.weather.split("转")[0] + ".png",
             temp: item.temperature.replace("/","~")
           }
           arr.push(data)
@@ -90,8 +126,10 @@ Page({
         this.setData({
           forecasts: arr
         })
+        if(cb){
+          cb()
+        }
       }
     })
-    console.log("ok")
   }
 })
